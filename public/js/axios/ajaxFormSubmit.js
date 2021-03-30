645370c6b1dd9ad47bc182e5cc530d4f9e04657a
ShekhSaifuddin007 <saifsabbir4@gmail.com>
2021-03-30 19:25:53 +0600

    set crf protection in axios header

--- public/js/axios/ajaxFormSubmit.js
+++ public/js/axios/ajaxFormSubmit.js
@@ -1,3 +1,6 @@
+// axios header setup
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector("meta[name='csrf-token']").getAttribute('content');
 
 // Ajax form submit logic
 function submitAjaxForm(selector, disable, redirect = null) {
